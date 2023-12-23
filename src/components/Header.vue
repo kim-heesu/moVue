e3df752e1847c717712b4ec2ff6ed2da156b9df8
kim-heesu <cjmgmltn@naver.com>
2023-12-24 03:10:17 +0900

    dd

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <header>
-    <Logo />ㅇㅇ
+  <header>dd
+    <Logo />
     <div class="nav nav-pills">
       <div class="nav-item" v-for="nav in navigations" :key="nav.name">
         <router-link class="nav-link" :to="nav.href" active-class="active" :class="{active: isMatch(nav.path)}">{{ nav.name }}</router-link>
