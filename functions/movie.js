f0e16250e101acfd2df6406cddb73e89ac4c06ad
kim-heesu <cjmgmltn@naver.com>
2023-12-26 00:47:28 +0900

    환경변수 테스트

--- functions/movie.js
+++ functions/movie.js
@@ -1,10 +1,9 @@
 const axios = require('axios')
+const { OMDB_API_KEY } = process.env
 
 exports.handler = async function (event){
-    console.log(event)
     const payload = JSON.parse(event.body)
     const {title,type,year,page,id} = payload
-    const OMDB_API_KEY = 'c4603bb9'
     const url = id 
     ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
     : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
